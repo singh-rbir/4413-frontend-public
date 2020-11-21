463d1a737b6e43d9be8051c5f425e846eb8740d3
Rushil Patel <rushilp2311@gmail.com>
2020-11-21 16:06:57 -0500

    Refactored Code And Added Home Page

---
+++ src/components/Signin.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+
+const Signin = () => {
+  return (
+    <div className="text-center mh-100">
+      <form className="form-signin">
+        <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
+        <label for="inputEmail" className="sr-only">
+          Email address
+        </label>
+        <input
+          type="email"
+          id="inputEmail"
+          className="form-control"
+          placeholder="Email address"
+          required=""
+          autofocus=""
+        />
+        <label for="inputPassword" className="sr-only">
+          Password
+        </label>
+        <input
+          type="password"
+          id="inputPassword"
+          className="form-control"
+          placeholder="Password"
+          required=""
+        />
+        <div className="checkbox mb-3">
+          <label>
+            <input type="checkbox" value="remember-me" /> Remember me
+          </label>
+        </div>
+        <button className="btn btn-lg btn-primary btn-block" type="submit">
+          Sign in
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Signin;
